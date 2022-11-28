5fdfda16444e1785264c2408de7b1cd197e86860
Matt Robbins <matthew.robbins@gmail.com>
2022-11-28 08:55:47 -0500

    adding new tail function

---
+++ pytail.py
@@ -0,0 +1,28 @@
+import os
+import sys
+def tail(f, n=1, bsize=4098):
+    """Tail a file and get X lines from the end"""
+    # place holder for the lines found
+    lines = []
+    offset = -bsize
+
+    # loop until we find n lines
+    while len(lines) < n:
+        try:
+            f.seek(offset, os.SEEK_END)
+        except IOError:  # either file is too small, or too many lines requested
+            f.seek(0)
+            lines = f.readlines()
+            break
+
+        lines = f.readlines()
+        offset -= bsize
+
+    return lines[-n:]
+
+if __name__ == "__main__":
+
+    with open(sys.argv[1],'r') as f:
+        lines = tail(f,10)
+        print(lines)
+
