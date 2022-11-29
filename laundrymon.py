a7136560301fa44e907197f16b2a11ae4d2d460b
Matt Robbins <matthew.robbins@gmail.com>
2022-11-29 08:06:04 -0500

    added missing pip requirements

--- laundrymon.py
+++ laundrymon.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
-from turbo_flask import Turbo
 import getstatus
 
 app = Flask(__name__)
-turbo = Turbo(app)
 
 STATUS = ["none","wash","dry","both"]
 @app.route("/")
